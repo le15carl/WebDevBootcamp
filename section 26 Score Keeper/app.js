b078e139939123e6a3f2428215a953d2e2aec827
Lauren Carleton <54991632+le15carl@users.noreply.github.com>
2022-04-22 21:49:48 -0400

    finished styling

--- section 26 Score Keeper/app.js
+++ section 26 Score Keeper/app.js
@@ -26,6 +26,10 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -36,6 +40,10 @@ p2Button.addEventListener('click', function () {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -61,4 +69,8 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
